e01d89a3a241224ab83d600c4235bbc2cf59612d
Kizinho <adikekizinho@gmail.com>
2020-03-17 01:54:43 +0100

    tag seo

--- resources/views/pages/upload.blade.php
+++ resources/views/pages/upload.blade.php
@@ -1,28 +1,27 @@
 @section('title')
 <title>MP3 tag editor - tag mp3 files online | the best mp3 tagger 2020</title>
-<meta name="description" content="Tagmp3.net is a free mp3 tag editor available online where you can tag mp3 files, change id3 tags, edit mp3 image, add or change existing album art in mp3 and change mp3 cover image." />
+<meta name="description" content="Mp3Tager.com is a free mp3 tag editor available online where you can tag mp3 files, change id3 tags, edit mp3 image, add or change existing album art in mp3 and change mp3 cover image." />
 <meta name="keywords" content="tagmp3, mp3tag, tag mp3, mp3 tag" />
-<meta name="apple-mobile-web-app-title" content="Tagmp3" />
-<meta property="fb:app_id" content="2291526731101380" />
-<meta http-equiv="Cache-control" content="public">
+<meta name="apple-mobile-web-app-title" content="Mp3Tager" />
+<meta property="fb:app_id" content="" />
 <meta name="theme-color" content="#d1ecf1"/>
-<meta property="og:title" content="MP3 tag editor - tag mp3 files online | tagmp3.net" />
-<meta property="og:description" content="Tagmp3.net is a free mp3 tag editor available online where you can tag mp3 files, change id3 tags, edit mp3 image, add or change existing album art in mp3 and change mp3 cover image." />
-<meta property="og:url" content="https://tagmp3.net/" />
+<meta property="og:title" content="MP3 tag editor - tag mp3 files online | mp3tager.com" />
+<meta property="og:description" content="Mp3Tager.com is a free mp3 tag editor available online where you can tag mp3 files, change id3 tags, edit mp3 image, add or change existing album art in mp3 and change mp3 cover image." />
+<meta property="og:url" content="{{url('/')}}" />
 <meta property="og:type" content="website" />
-<meta property="og:site_name" content="Tagmp3" />
+<meta property="og:site_name" content="Mp3Tager" />
 <meta name="twitter:card" content="summary">
-<meta name="twitter:site" content="@Tagmp31">
-<meta name="twitter:title" content="MP3 tag editor - tag mp3 files online | tagmp3.net">
-<meta name="twitter:description" content="Tagmp3.net is a free mp3 tag editor available online where you can tag mp3 files, change id3 tags, edit mp3 image, add or change existing album art in mp3 and change mp3 cover image.">
-<link rel="canonical" href="https://tagmp3.net/" />
-<meta property="og:image" content="https://tagmp3.net/images/tagmp3-1200x630.png" />
-<meta property="og:image:alt" content="MP3 tag editor - tag mp3 files online | tagmp3.net">
+<meta name="twitter:site" content="@mp3tager">
+<meta name="twitter:title" content="MP3 tag editor - tag mp3 files online | mp3tager.com">
+<meta name="twitter:description" content="Mp3Tager.com is a free mp3 tag editor available online where you can tag mp3 files, change id3 tags, edit mp3 image, add or change existing album art in mp3 and change mp3 cover image.">
+<link rel="canonical" href="{{url('/')}}" />
+<meta property="og:image" content="{{asset('logo/logo.png') }}" />
+<meta property="og:image:alt" content="MP3 tag editor - tag mp3 files online | mp3tager.com">
 <meta property="og:image:type" content="image/png" />
 <meta property="og:image:width" content="1200" />
 <meta property="og:image:height" content="630" />
-<meta name="twitter:image" content="https://tagmp3.net/images/tagmp3-1200x630.png" />
-<meta name="twitter:image:alt" content="MP3 tag editor - tag mp3 files online | tagmp3.net" />
+<meta name="twitter:image" content="{{asset('logo/logo.png') }}" />
+<meta name="twitter:image:alt" content="MP3 tag editor - tag mp3 files online | mp3tager.com" />
 
 <link rel="stylesheet" href="{{asset('css/upload.css')}}">
 <link rel="stylesheet" href="{{asset('dropzone/dropzone.min.css')}}">
