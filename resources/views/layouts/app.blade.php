1600b4610bf3891a7a2e52e55ef7d06a953f500f
Kizinho <adikekizinho@gmail.com>
2020-03-23 13:44:30 +0100

    manifest

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+     <link rel="manifest" href="{{url('/manifest.json')}}">
     <!-- Favicons -->
     <link rel="icon" href="{{asset('logo/icon.png') }}">
     <link rel="shortcut icon" href="{{asset('logo/icon.png') }}">
