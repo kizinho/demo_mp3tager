249e7ecdd59d417d8b9aa91cdf0c792e2f127843
Kizinho <adikekizinho@gmail.com>
2020-04-18 09:53:51 +0100

    new upda

--- resources/views/pages/download.blade.php
+++ resources/views/pages/download.blade.php
@@ -24,7 +24,7 @@
 <meta name="twitter:image" content="{{asset('logo/logo.png') }}" />
 <meta name="twitter:image:alt" content="Upload mp3 -  join two mp3 files online | mp3 tager for editing mp3 files" />
 <link rel="stylesheet" href="{{asset('css/download.css')}}">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 @endsection
 @extends('layouts.app')
 @section('content')
@@ -44,30 +44,57 @@
             <span class="badge badge-danger">{{$download->size}}</span> or    
             <button class="badge badge-primary down-btn-item clip-btn{{$key}}" id=''> copy <i class="fas fa-copy"></i></button> link to clipborad 
             <br/>
-            <a href="{{url('tags?' . $key . '='.$download->slug)}}"> Edit  <i class="fa fa-pen"></i></a></div> 
+            <a href="{{url('tags?' . $key . '='.$download->slug)}}"> Edit  <i class="fa fa-pen"></i></a>
+            <br/> <br/>
+            <audio controls>
+                <source src="{{url('tag-downloads/'.$download->slug)}}" type="audio/ogg">
+                <source src="{{url('tag-downloads/'.$download->slug)}}" type="audio/mpeg">
+                Your browser does not support the audio element.
+            </audio>
+            <br/> <br/>
+            <textarea cols="50" rows="5" id="textarea{{$key}}">
+                <audio controls>
+  <source src="{{url('tag-downloads/'.$download->slug)}}" type="audio/ogg">
+  <source src="{{url('tag-downloads/'.$download->slug)}}" type="audio/mpeg">
+Your browser does not support the audio element.
+</audio>
+
+            </textarea>
+            <br>
+            <button onclick="copy{{$key}}()">Copy</button>
+        </div> 
         <input type="text"  style="opacity: 0;" id="down-link-item{{$key}}">
-  
 
         <script>
+            function copy{{$key}}() {
+                let textarea = document.getElementById("textarea{{$key}}");
+                textarea.select();
+                document.execCommand("copy");
+                 let message = "Embedded Code Copied Successfully , you can use it on your website for post";
+                    toastr.options.onHidden = function () {
 
-$(document).ready(function () {
-    $('#down-link-item{{$key}}').val($('.lk{{$key}} a').attr('href'));
-    $('.clip-btn{{$key}}').click(function () {
-        $('#down-link-item{{$key}}').val($('.lk{{$key}} a').attr('href'));
-        var copyText = document.getElementById("down-link-item{{$key}}");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        let message = "Copied the text: " + copyText.value;
-        toastr.options.onHidden = function () {
+                    };
+                    toastr.success(message, {timeOut: 50000});
+            }
 
-        };
-        toastr.success(message, {timeOut: 50000});
+            $(document).ready(function () {
+                $('#down-link-item{{$key}}').val($('.lk{{$key}} a').attr('href'));
+                $('.clip-btn{{$key}}').click(function () {
+                    $('#down-link-item{{$key}}').val($('.lk{{$key}} a').attr('href'));
+                    var copyText = document.getElementById("down-link-item{{$key}}");
+                    copyText.select();
+                    copyText.setSelectionRange(0, 99999);
+                    document.execCommand("copy");
+                    let message = "Copied the text: " + copyText.value;
+                    toastr.options.onHidden = function () {
 
-    });
+                    };
+                    toastr.success(message, {timeOut: 50000});
+
+                });
 
 
-});
+            });
         </script>
         @endforeach
         @if(count($details) >1 )
