fe808f85a404961baa99563df4086114c1bb3023
Kizinho <adikekizinho@gmail.com>
2020-04-20 07:33:42 +0100

    fix going to naijacrawl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,25 +30,29 @@ class AppServiceProvider extends ServiceProvider {
         date_default_timezone_set('Africa/Lagos');
         view()->composer('*', function ($token) {
             $token = session('token');
-            if (Cache::has($token)) {
-                $res = Cache::get($token);
-            } else {
-                $headers = [
-                    'Authorization' => $token
-                ];
-                $client = new Client();
-                $url = config('app.naijacrawl_api') . '/token';
-                $response = $client->request('GET', $url, [
-                    'headers' => $headers
-                ]);
-
-                $res = json_decode($response->getBody());
-                Cache::put($token, $res, 525600);
-            }
-            if (empty($res)) {
+            if (empty($token)) {
                 View::share('user', false);
             } else {
-                View::share('user', $res->data);
+                if (Cache::has($token)) {
+                    $res = Cache::get($token);
+                } else {
+                    $headers = [
+                        'Authorization' => $token
+                    ];
+                    $client = new Client();
+                    $url = config('app.naijacrawl_api') . '/token';
+                    $response = $client->request('GET', $url, [
+                        'headers' => $headers
+                    ]);
+
+                    $res = json_decode($response->getBody());
+                    Cache::put($token, $res, 525600);
+                }
+                if (empty($res)) {
+                    View::share('user', false);
+                } else {
+                    View::share('user', $res->data);
+                }
             }
         });
     }
