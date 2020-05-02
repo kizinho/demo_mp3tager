a3c2ddf7a0d4d80b566322fa2f2a06a2ff7d4bc2
Kizinho <adikekizinho@gmail.com>
2020-05-02 11:38:12 +0100

    nkjkhhh

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -122,7 +122,7 @@ class UploadController extends Controller {
             ]);
 
             $res = json_decode($response->getBody());
-            dd($res);
+
             if ($res->status == 401) {
                 session()->flash('message.level', 'error');
                 session()->flash('message.color', 'red');
@@ -232,7 +232,7 @@ class UploadController extends Controller {
             ]);
 
             $res = json_decode($response->getBody());
-            //dd($res);
+            dd($res);
             if ($res->status == 401) {
                 session()->flash('message.level', 'error');
                 session()->flash('message.color', 'red');
